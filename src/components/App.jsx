01860554098a2bf58439439d3abb7a1f9c558f66
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-06-07 19:30:33 +0300

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,68 +4,50 @@ import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
-
 export default function App() {
- const [good, setGood] = useState(0);
+  const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  
-  //початковий стан
- // state = {
- //   good: 0,
- //   neutral: 0,
- //   bad: 0,
- // };
 
-const leaveFeedback = event => {
-   setGood(prevState => event.prevState + 1);
-   setBad(prevState => event.prevState + 1);
-   setNeutral(prevState => event.prevState + 1);
- //  this.setState({ [event]: this.state[event] + 1 });
+  const leaveFeedback = event => {
+    switch (event) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
-  
-  
-  
   //загальнf кількість зібраних відгуків
-  const countTotalFeedback = () => 
-  //  return this.state.bad + this.state.neutral + this.state.good;
+  const countTotalFeedback = () =>
     good + neutral + bad;
-
   //відсоток позитивних відгуків
   const countPositiveFeedbackPercentage = () =>
     Math.round((good * 100) / countTotalFeedback());
   
-  //{
-  //  let percentage = 0;
-  //  if (this.countTotalFeedback() !== 0) {
-  //    percentage = Number.parseInt(
-  //      (this.state.good / this.countTotalFeedback()) * 100
-  //    );
-  //  }
-  //  return percentage;
-  //};
-
-  //рендер всього контенту
- // render() {
-   // const { good, neutral, bad } = this.state;
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys({ good, neutral, bad })}
-            onLeaveFeedback={leaveFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercentage()}
-          ></Statistics>
-        </Section>
-      </div>
-    );
-  //}
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys({ good, neutral, bad })}
+          onLeaveFeedback={leaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        ></Statistics>
+      </Section>
+    </div>
+  );
 }
